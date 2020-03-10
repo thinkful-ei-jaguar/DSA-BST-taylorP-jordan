7c01d492af1cc70e6bccdb9cfe24d364951102e0
Jordan Castillo <jordanxcallaway@gmail.com>
2020-03-10 09:35:28 -0700

    bst class find method

--- otherDrills.js
+++ otherDrills.js
@@ -3,36 +3,56 @@ class BinarySearchTree {
     this.key = key,
     this.parent= parent,
     this.right = null,
-    this.left= null
+    this.left= null;
   }
 
   insert(item){
     //if tree is empty
     if(this.key === null) {
-      this.key = item
+      this.key = item;
     }
     else if(item<this.key){
       if(this.left === null){
-        this.left = new BinarySearchTree(item, this)
+        this.left = new BinarySearchTree(item, this);
       } else {
-        this.left.insert(item)
+        this.left.insert(item);
       }
     }
     else if(item>this.key){
       if(this.right === null){
-        this.right = new BinarySearchTree(item, this)
+        this.right = new BinarySearchTree(item, this);
       } else {
-        this.right.insert(item)
+        this.right.insert(item);
       }
+    }
+  }
+
+  find(item) {
+    //if the item we are finding is the root node 
+    if (this.key == item) {
+      return this;
+    }
+    //if the item we are looking for is less than the root, the go to the left-hand branch
+    //if there is an exisiting left child, recursively search for the item 
+    else if (item < this.key && this.left) {
+      return this.left.find(item);
+    }
+    else if (item > this.key && this.right) {
+      return this.right.find(item);
+    }
+    else {
+      throw new Error('Cannot find the item')
     }
   }
 }
 
 function main() {
-  let  bst = new BinarySearchTree()
+  let  bst = new BinarySearchTree();
 
-  bst.insert(5)
-  bst.insert(3)
-  bst.insert(2)
-  bst.insert(4)
+  bst.insert(5);
+  bst.insert(3);
+  bst.insert(2);
+  bst.insert(4);
+
+  bst.find(2);
 }
